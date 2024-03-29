0c50ca3d4fa783366e9e2ce31bee2b8460dc348f
Andrea <andrea.soriano@outlook.com>
2024-03-29 11:45:59 +0800

    added JS functions
    added function to change city and reflect current temp

--- src/boop.js
+++ src/boop.js
@@ -1,8 +1,22 @@
+function updateWeatherInfo(response) {
+  let temp = document.querySelector("#current-temp");
+  temp.innerHTML = Math.round(response.data.temperature.current);
+}
+
+function searchCity(city) {
+  //make API call and update UI
+  //Separation of concerns, functions should only do 1 thing and do it well
+  let apiKey = "7284tb793b63a4fb3a7aacc794o38a02";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateWeatherInfo);
+}
+
 function submitSearchForm(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
   let cityElement = document.querySelector("#current-city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
